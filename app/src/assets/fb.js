a960e58ee4cc6ebef6a76d9347702e7382c8d083
Daniel Büchele <daniel@buechele.cc>
2017-01-16 00:21:13 +0100

    removing react-dev-tools

--- app/src/assets/fb.js
+++ app/src/assets/fb.js
@@ -1,6 +1,3 @@
-// React dev tools hook
-Object.defineProperty(window, '__REACT_DEVTOOLS_GLOBAL_HOOK__', {value: {_renderers: {},helpers: {},inject: function(renderer) {var id = Math.random().toString(16).slice(2);this._renderers[id] = renderer;this.emit('renderer', {id, renderer});},_listeners: {},sub: function(evt, fn) {this.on(evt, fn);return function () {this.off(evt, fn)};},on: function(evt, fn) {if (!this._listeners[evt]) {this._listeners[evt] = [];}this._listeners[evt].push(fn);},off: function(evt, fn) {if (!this._listeners[evt]) {return;}var ix = this._listeners[evt].indexOf(fn);if (ix !== -1) {this._listeners[evt].splice(ix, 1);}if (!this._listeners[evt].length) {this._listeners[evt] = null;}},emit: function(evt, data) {if (this._listeners[evt]) {this._listeners[evt].map(function fn() {fn(data)});}}}});
-
 const { ipcRenderer, shell } = require('electron');
 const config = require('../config');
 const latestMessages = new Map();
